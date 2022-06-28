fbcd096a5f36457036165325c0175a9cb1418d47
Jorge Garit <jorgegarit@gmail.com>
2022-06-27 22:36:48 -0400

    added multiple rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,8 +17,7 @@ var enemyAttack = 12;
 
 
 var fight = function (enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Fladiators!");
+    while(enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     console.log(promptFight);
@@ -74,9 +73,12 @@ var fight = function (enemyName) {
     else {
         window.alert("You need to choose a valid option. Try again!");
     }
+    }
 }
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
 
